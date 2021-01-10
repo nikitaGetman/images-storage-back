dec5cd6a92af20ff8b0e4ca52f8b95be1141a75f
Nikita Getman <nikita.getman56@gmail.com>
2021-01-11 01:30:25 +0700

    download from web plugin

---
+++ plugins/uploadFromWeb.js
@@ -0,0 +1,50 @@
+const cheerio = require("cheerio");
+const fs = require("fs");
+const request = require("request");
+const uuid = require("uuid");
+const BasePlugin = require("./static/base");
+
+const PLUGIN_NAME = "download-from-web";
+
+function downloadImage(uri, name = uuid.v4(), callback = () => {}) {
+  const folder = "./database";
+  const extension = uri.split(".").pop();
+  const filePath = `${folder}/${name}.${extension}`;
+  request(uri).pipe(fs.createWriteStream(filePath)).on("close", callback);
+}
+
+class UploadFromWebPlugin extends BasePlugin {
+  constructor(db) {
+    super(PLUGIN_NAME, db);
+    this.registerRoutes();
+  }
+
+  registerRoutes() {
+    this.router.get(`/${PLUGIN_NAME}`, this.downloadImagesFromUrl);
+    this.router.post(`/${PLUGIN_NAME}`, this.downloadImagesFromUrl);
+  }
+
+  downloadImagesFromUrl(req, res) {
+    const url = req.params.url || req.query.url;
+    const images = [];
+
+    request(url, function (err, _, body) {
+      if (err) throw err;
+      const $ = cheerio.load(body);
+
+      $("img").each((_, el) => {
+        const src = $(el).attr("src");
+
+        if (src && !src.includes("svg")) {
+          images.push(src);
+        }
+      });
+
+      images.forEach((src) => downloadImage(src));
+
+      res.send(images);
+    });
+  }
+}
+
+module.exports = UploadFromWebPlugin;
