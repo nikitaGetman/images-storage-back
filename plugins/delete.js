dd8065138a0c9072203989bdefee328c22b68290
nikitaGetman <nikita.getman56@gmail.com>
2021-02-22 02:57:55 +0700

    minor fixes

---
+++ plugins/delete.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const checkAuth = require("../middlewares/checkAuth");
+const BasePlugin = require("./static/base");
+
+const PLUGIN_NAME = "images-deletion";
+
+class DeletePlugin extends BasePlugin {
+  constructor(db) {
+    super(PLUGIN_NAME, db);
+    this.registerRoutes();
+  }
+
+  registerRoutes() {
+    this.router.delete(`/${PLUGIN_NAME}`, checkAuth, (req, res) =>
+      this.deleteImage(req, res)
+    );
+  }
+
+  deleteImage(req, res) {
+    const { image } = req.body;
+    const filePath = `./database/${req.userId}/${image}`;
+
+    fs.unlink(filePath, (err) => {
+      if (err && err.code == "ENOENT") {
+        res.status(400).send({ error: "No such file." });
+      } else if (err) {
+        res.status(500).send({ error: err });
+      } else {
+        res.send({ message: "ok" });
+      }
+    });
+  }
+}
+
+module.exports = DeletePlugin;
